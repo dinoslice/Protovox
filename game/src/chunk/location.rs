b146919e84a270e56aee8cb082dcec81d9ff1e20
abscosmos <joshua@dinoslice.com>
2024-07-31 13:32:06 -0500

    Derive debug for ChunkLocation

--- game/src/chunk/location.rs
+++ game/src/chunk/location.rs
@@ -2,7 +2,7 @@ use glm::TVec3;
 use crate::chunk;
 use crate::location::WorldLocation;
 
-#[derive(Default, Eq, PartialEq, Clone)]
+#[derive(Default, Eq, PartialEq, Clone, Debug)]
 pub struct ChunkLocation(pub TVec3<i32>);
 
 impl From<&WorldLocation> for ChunkLocation {
