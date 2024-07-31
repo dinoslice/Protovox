60423196034b5e4af7fdd50caa935d20507b4fcb
abscosmos <joshua@dinoslice.com>
2024-07-31 16:16:25 -0500

    Implement Default for ChunkData

--- game/src/chunk/data.rs
+++ game/src/chunk/data.rs
@@ -6,6 +6,12 @@ pub struct ChunkData {
     pub blocks: [Block; BLOCKS_PER_CHUNK],
 }
 
+impl Default for ChunkData {
+    fn default() -> Self {
+        Self { blocks: [Block::Air; BLOCKS_PER_CHUNK] }
+    }
+}
+
 impl ChunkData {
     pub fn set_block(&mut self, pos: ChunkPos, block: Block) {
         self.blocks[pos.0 as usize] = block;
