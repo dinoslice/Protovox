a72376ea4c95128dbffda91f59f7cd7d2c4d3b55
abscosmos <joshua@dinoslice.com>
2024-07-31 16:16:41 -0500

    Trait to render chunks as voxels

---
+++ client/src/rendering/voxel_renderable.rs
@@ -0,0 +1,32 @@
+use game::block::Block;
+use game::chunk::data::ChunkData;
+use game::chunk::pos::ChunkPos;
+use crate::rendering::face_data::{FaceData, FaceType};
+
+pub trait VoxelRenderable {
+    fn as_faces(&self) -> Vec<FaceData>;
+}
+
+impl VoxelRenderable for ChunkData {
+    fn as_faces(&self) -> Vec<FaceData> {
+        let non_air_block_count = self.blocks
+            .iter()
+            .filter(|&&b| b != Block::Air)
+            .count();
+
+        let mut face_data = Vec::with_capacity(non_air_block_count);
+
+        for (pos, _) in self.blocks.iter().enumerate().filter(|(_, &b)| b != Block::Air) {
+            let pos = pos.try_into().unwrap();
+
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Top));
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Bottom));
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Left));
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Right));
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Front));
+            face_data.push(FaceData::new(ChunkPos(pos), FaceType::Back));
+        }
+
+        face_data
+    }
+}
