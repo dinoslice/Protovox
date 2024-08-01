42505d2b423c507df865ad7391f7f911be3d2095
abscosmos <joshua@dinoslice.com>
2024-08-01 14:30:58 -0500

    Get adjacent chunk positions

--- game/src/chunk/mod.rs
+++ game/src/chunk/mod.rs
@@ -1,6 +1,7 @@
 pub mod pos;
 pub mod data;
 pub mod location;
+mod adjacent;
 
 pub const CHUNK_SIZE: glm::TVec3<u8> = glm::TVec3::new(32, 64, 32);
 pub const BLOCKS_PER_CHUNK: usize = (32 * 64 * 32) as _;
