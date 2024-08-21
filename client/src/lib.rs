36ae36110ebd2b711fd390251148f746f79d2872
abscosmos <joshua@dinoslice.com>
2024-08-20 21:42:47 -0500

    Fix module visibility

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,8 @@
 extern crate nalgebra as na;
 extern crate nalgebra_glm as glm;
 
-pub mod rendering;
-pub mod input;
 pub mod application;
-pub mod camera;
+
+mod rendering;
+mod input;
+mod camera;
