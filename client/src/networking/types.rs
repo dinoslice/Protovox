edf2af8e9f1acf5723c0f6d7bbc85082a6ed3fc2
abscosmos <joshua@dinoslice.com>
2024-09-16 10:40:46 -0500

    Add stuff required for packets

---
+++ client/src/networking/types.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+use strum::FromRepr;
+use packet::PacketHeader;
+
+#[repr(u16)]
+#[derive(Debug, Serialize, Deserialize, FromRepr, Copy, Clone)]
+pub enum PacketType {
+    ConnectionRequest,
+
+    ChunkGenRequestEvent,
+    ChunkGenEvent,
+
+    RenderDistanceRequestEvent,
+    RenderDistanceUpdateEvent,
+
+    ClientInformationRequestEvent,
+    ClientInformationUpdateEvent,
+
+    ClientSettingsRequestEvent,
+    ClientSettingsUpdateEvent,
+}
+
+impl PacketHeader for PacketType {
+    fn repr(&self) -> u16 {
+        *self as _
+    }
+
+    #[inline]
+    fn from_header_repr(repr: u16) -> Option<Self> {
+        Self::from_repr(repr)
+    }
+}
