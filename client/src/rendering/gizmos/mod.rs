1667e56965dadcb5d31654b113885962c5adf963
abscosmos <joshua@dinoslice.com>
2024-10-19 17:38:36 -0500

    Render BoxGizmos by decomposing them to LineGizmos

--- client/src/rendering/gizmos/mod.rs
+++ client/src/rendering/gizmos/mod.rs
@@ -1,4 +1,5 @@
-use shipyard::{IntoWorkload, UniqueView, UniqueViewMut, ViewMut, Workload};
+use glm::Vec3;
+use shipyard::{EntitiesViewMut, IntoWorkload, UniqueView, UniqueViewMut, ViewMut, Workload};
 use wgpu::util::DeviceExt;
 use settings::read_settings;
 use line_render_state::initialize_line_gizmos_render_state;
@@ -24,6 +25,7 @@ pub fn initialize() -> Workload {
 pub fn update() -> Workload {
     (
         process_line_gizmos,
+        decompose_box_gizmos,
     ).into_workload()
 }
 
@@ -49,4 +51,34 @@ pub fn process_line_gizmos(g_ctx: UniqueView<GraphicsContext>, mut line_gizmo_re
     });
 
     line_gizmo_rend_state.update_buffer(&g_ctx, &buf);
+}
+
+pub fn decompose_box_gizmos(mut entities: EntitiesViewMut, mut vm_box_gizmos: ViewMut<BoxGizmo>, mut vm_line_gizmos: ViewMut<LineGizmo>) {
+    for BoxGizmo { min, max, style, lifetime } in vm_box_gizmos.drain() {
+        let corners = [
+            Vec3::new(min.x, min.y, min.z),
+            Vec3::new(min.x, min.y, max.z),
+            Vec3::new(min.x, max.y, min.z),
+            Vec3::new(min.x, max.y, max.z),
+            Vec3::new(max.x, min.y, min.z),
+            Vec3::new(max.x, min.y, max.z),
+            Vec3::new(max.x, max.y, min.z),
+            Vec3::new(max.x, max.y, max.z),
+        ];
+
+        let edges = [
+            (0, 1), (0, 2), (1, 3), (2, 3), // bottom
+            (4, 5), (4, 6), (5, 7), (6, 7), // top
+            (0, 4), (1, 5), (2, 6), (3, 7), // vertical
+        ];
+
+        let map = edges.into_iter().map(|(start, end)| LineGizmo {
+            start: corners[start],
+            end: corners[end],
+            style: style.clone(),
+            lifetime: lifetime.clone(),
+        });
+
+        entities.bulk_add_entity(&mut vm_line_gizmos, map);
+    }
 }
