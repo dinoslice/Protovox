dab2cab4d4850d25406c4a0aaa6265713a2841b5
abscosmos <joshua@dinoslice.com>
2024-11-09 19:39:36 -0600

    Create gamemode checks for run_if/skip_if

--- client/src/gamemode.rs
+++ client/src/gamemode.rs
@@ -1,8 +1,27 @@
-use shipyard::Component;
+use shipyard::{Component, IntoIter, View};
+use crate::components::LocalPlayer;
 
 #[derive(Clone, Component, Debug, Default, Eq, PartialEq)]
 pub enum Gamemode {
     #[default]
     Survival,
     Spectator,
+}
+
+pub fn local_player_is_gamemode_survival(v_local_player: View<LocalPlayer>, v_gamemode: View<Gamemode>) -> bool {
+    let (_, gamemode) = (&v_local_player, &v_gamemode)
+        .iter()
+        .next()
+        .expect("local player should have gamemode");
+    
+    *gamemode == Gamemode::Survival
+}
+
+pub fn local_player_is_gamemode_spectator(v_local_player: View<LocalPlayer>, v_gamemode: View<Gamemode>) -> bool {
+    let (_, gamemode) = (&v_local_player, &v_gamemode)
+        .iter()
+        .next()
+        .expect("local player should have gamemode");
+
+    *gamemode == Gamemode::Spectator
 }
