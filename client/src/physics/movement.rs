7295ca68fd92f45603601f181a81d91154ddf50c
abscosmos <joshua@dinoslice.com>
2024-11-09 19:40:24 -0600

    Update movement for spectator

--- client/src/physics/movement.rs
+++ client/src/physics/movement.rs
@@ -3,15 +3,25 @@ use glm::{RealNumber, Vec2, Vec3};
 use shipyard::{IntoIter, UniqueView, View, ViewMut};
 use na::SVector;
 use crate::application::delta_time::LastDeltaTime;
-use crate::components::{IsOnGround, LocalPlayer, PlayerSpeed, Transform, Velocity};
+use crate::components::{IsOnGround, LocalPlayer, PlayerSpeed, SpectatorSpeed, Transform, Velocity};
 use crate::gamemode::Gamemode;
 use crate::input::action_map::Action;
 use crate::input::InputManager;
 
-pub fn process_movement(input: UniqueView<InputManager>, delta_time: UniqueView<LastDeltaTime>, v_local_player: View<LocalPlayer>, mut vm_transform: ViewMut<Transform>, mut vm_velocity: ViewMut<Velocity>, v_player_speed: View<PlayerSpeed>, v_is_on_ground: View<IsOnGround>, v_gamemode: View<Gamemode>) {
+pub fn process_movement(
+    input: UniqueView<InputManager>,
+    delta_time: UniqueView<LastDeltaTime>,
+    v_local_player: View<LocalPlayer>,
+    v_transform: View<Transform>,
+    mut vm_velocity: ViewMut<Velocity>,
+    v_player_speed: View<PlayerSpeed>,
+    v_spectator_speed: View<SpectatorSpeed>,
+    v_is_on_ground: View<IsOnGround>,
+    v_gamemode: View<Gamemode>,
+) {
     let dt_secs = delta_time.0.as_secs_f32();
 
-    let (_, transform, velocity, player_speed, is_on_ground, gamemode) = (&v_local_player, &mut vm_transform, &mut vm_velocity, &v_player_speed, &v_is_on_ground, &v_gamemode)
+    let (_, transform, velocity, player_speed, spectator_speed, is_on_ground, gamemode) = (&v_local_player, &v_transform, &mut vm_velocity, &v_player_speed, &v_spectator_speed, &v_is_on_ground, &v_gamemode)
         .iter()
         .next()
         .expect("TODO: local player didn't have transform, velocity, player speed");
@@ -22,7 +32,7 @@ pub fn process_movement(input: UniqueView<InputManager>, delta_time: UniqueView<
                 input.pressed().get_axis(Action::MoveForward, Action::MoveBackward) as f32,
                 input.pressed().get_axis(Action::MoveRight, Action::MoveLeft) as f32,
             );
-            
+
             let xz = match input_vec.try_normalize(f32::EPSILON) {
                 Some(norm_input) => {
                     let plane_dir = glm::rotate_vec2(&norm_input, transform.yaw);
@@ -50,12 +60,12 @@ pub fn process_movement(input: UniqueView<InputManager>, delta_time: UniqueView<
             let xyz = match input_vec.try_normalize(f32::EPSILON) {
                 Some(norm_input) => {
                     let plane_dir = glm::rotate_vec2(&norm_input.xz(), transform.yaw);
-                    
-                    let target = Vec3::new(plane_dir[0], norm_input.y, plane_dir[1]);
 
-                    move_towards(&velocity.0, &(target * player_speed.max_vel), player_speed.accel * dt_secs)
+                    let target = Vec3::new(plane_dir.x, norm_input.y, plane_dir.y);
+
+                    move_towards(&velocity.0, &(target * spectator_speed.curr_speed), spectator_speed.accel() * dt_secs)
                 }
-                None => move_towards(&velocity.0, &Vec3::zeros(), player_speed.friction * dt_secs),
+                None => move_towards(&velocity.0, &Vec3::zeros(), velocity.0.magnitude() / spectator_speed.friction_time * dt_secs),
             };
 
             velocity.0 = xyz;
@@ -80,8 +90,8 @@ pub fn apply_camera_input(input: UniqueView<InputManager>, delta_time: UniqueVie
     transform.pitch = transform.pitch.clamp(-SAFE_FRAC_PI_2, SAFE_FRAC_PI_2);
 }
 
-pub fn adjust_fly_speed(input: UniqueView<InputManager>, v_local_player: View<LocalPlayer>, mut vm_player_speed: ViewMut<PlayerSpeed>) {
-    let (_, player_speed) = (&v_local_player, &mut vm_player_speed)
+pub fn adjust_spectator_fly_speed(input: UniqueView<InputManager>, v_local_player: View<LocalPlayer>, mut vm_spectator_speed: ViewMut<SpectatorSpeed>) {
+    let (_, spectator_speed) = (&v_local_player, &mut vm_spectator_speed)
         .iter()
         .next()
         .expect("TODO: local player didn't have player speed to modify");
@@ -89,17 +99,20 @@ pub fn adjust_fly_speed(input: UniqueView<InputManager>, v_local_player: View<Lo
     const SCROLL_SCALE: f32 = 0.32;
     const SCROLL_THRESHOLD: f32 = 0.2;
 
-    player_speed.max_vel = match input.mouse_manager.scroll.partial_cmp(&0.0).unwrap_or(Ordering::Equal) {
-        Ordering::Less => match player_speed.max_vel >= SCROLL_THRESHOLD {
-            true => player_speed.max_vel * (1.0 + SCROLL_SCALE),
+    spectator_speed.curr_speed = match input.mouse_manager.scroll.partial_cmp(&0.0).unwrap_or(Ordering::Equal) {
+        Ordering::Less => match spectator_speed.curr_speed >= SCROLL_THRESHOLD {
+            true => spectator_speed.curr_speed * (1.0 + SCROLL_SCALE),
             false => SCROLL_THRESHOLD,
         },
-        Ordering::Greater => match player_speed.max_vel >= SCROLL_THRESHOLD {
-            true => player_speed.max_vel * (1.0 - SCROLL_SCALE),
+        Ordering::Greater => match spectator_speed.curr_speed >= SCROLL_THRESHOLD {
+            true => spectator_speed.curr_speed * (1.0 - SCROLL_SCALE),
             false => 0.0,
         }
-        _ => player_speed.max_vel,
-    }.clamp(0.0, 125.0);
+        Ordering::Equal => spectator_speed.curr_speed,
+    }
+        .clamp(0.0, spectator_speed.maximum_speed);
+    
+    spectator_speed.curr_speed = spectator_speed.curr_speed.clamp(0.0, spectator_speed.maximum_speed);
 }
 
 pub fn move_towards<T: RealNumber, const N: usize> (current: &SVector<T, N>, target: &SVector<T, N>, max_dist: T) -> SVector<T, N> {
