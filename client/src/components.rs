8712d5079d4e54826a259dccbf4b3e12c38e0e00
abscosmos <joshua@dinoslice.com>
2024-11-09 19:40:56 -0600

    Change max spectator fly speed

--- client/src/components.rs
+++ client/src/components.rs
@@ -74,7 +74,7 @@ impl Default for SpectatorSpeed {
     fn default() -> Self {
         Self { // TODO: replace with better values
             curr_speed: 4.32,
-            maximum_speed: 96.0,
+            maximum_speed: 256.0,
             accel_time: 0.2,
             friction_time: 0.18,
         }
