34da48a3869f3d63f03edc7a52666a2f40c47bc7
abscosmos <joshua@dinoslice.com>
2024-12-12 09:49:46 -0600

    Create function to load texture atlas / texture array

--- client/src/rendering/texture.rs
+++ client/src/rendering/texture.rs
@@ -18,6 +18,84 @@ impl Texture {
         Ok( Self::from_image(device, queue, &img, Some(label)) )
     }
 
+    pub fn from_images_2d(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        images: &[image::DynamicImage],
+        label: Option<&str>
+    ) -> Option<Self> {
+        let dimensions = images.first()?.dimensions();
+
+        if images.iter().any(|img| img.dimensions() != dimensions) {
+            return None;
+        }
+
+        let size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: images.len() as _,
+        };
+
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                label,
+                size,
+                mip_level_count: 1,
+                sample_count: 1, // multisampling
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                // texture_binding -> use texture in shaders, copy_dst -> copy data into texture
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                view_formats: &[],
+            }
+        );
+
+
+        for (i, image) in images.into_iter().enumerate() {
+            let rgba = image.to_rgba8();
+
+            queue.write_texture(
+                wgpu::ImageCopyTexture {
+                    aspect: wgpu::TextureAspect::All,
+                    texture: &texture,
+                    mip_level: 0,
+                    origin: wgpu::Origin3d {
+                        z: i as u32,
+                        .. wgpu::Origin3d::ZERO
+                    },
+                },
+                &rgba,
+                wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(size_of::<image::Rgba<u8>>() as u32 * dimensions.0),
+                    rows_per_image: Some(dimensions.1),
+                },
+                wgpu::Extent3d {
+                    depth_or_array_layers: 1,
+                        .. size
+                }
+            );
+        }
+
+        // view into the texture to read from it
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        // sampler uses tex-coords (u, v) and sampler returns data from the texture
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge, // repeats the last pixel, to the end
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Nearest, // when close to the texture, use nearest when displaying
+                min_filter: wgpu::FilterMode::Nearest, // when far, use nearest when displaying
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
+        Some(Self { texture, view, sampler })
+    }
+
     pub fn from_image(
         device: &wgpu::Device,
         queue: &wgpu::Queue,
