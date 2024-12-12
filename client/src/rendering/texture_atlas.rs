46df5b75e9a6bf8c92e4907065baa9c7db85f93b
abscosmos <joshua@dinoslice.com>
2024-12-12 09:50:39 -0600

    Change TextureAtlas to use a texture array

--- client/src/rendering/texture_atlas.rs
+++ client/src/rendering/texture_atlas.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::num::NonZeroU32;
+use std::path::PathBuf;
 use shipyard::{AllStoragesView, Unique, UniqueView};
 use crate::rendering::texture::Texture;
 use crate::rendering::graphics_context::GraphicsContext;
@@ -5,15 +8,36 @@ use crate::rendering::graphics_context::GraphicsContext;
 #[derive(Unique)]
 #[allow(dead_code)]
 pub struct TextureAtlas {
-    pub texture: Texture,
+    pub texture_atlas: Texture,
     pub bind_group: wgpu::BindGroup,
     pub bind_group_layout: wgpu::BindGroupLayout,
 }
 
 pub fn initialize_texture_atlas(g_ctx: UniqueView<GraphicsContext>, storages: AllStoragesView) {
     // 4. load textures into bind group
-    let texture = Texture::from_bytes(&g_ctx.device, &g_ctx.queue, include_bytes!("../../assets/texture_atlas.png"), "texture_atlas.png")
-        .expect("atlas didn't exist");
+
+    let textures = [
+        "cobblestone",
+        "dirt",
+        "grass",
+        "grass_side",
+        "debug_red",
+        "debug_green",
+        "debug_blue",
+    ];
+
+    let loaded_textures = textures.map(|key| {
+        // TODO: pack textures into binary or better loading?
+        let path = String::from("client/assets/") + key + ".png";
+
+        tracing::debug!("{path}");
+        let bytes = fs::read(path).expect("TODO: better error; file to exist");
+
+        image::load_from_memory(&bytes).expect("TODO: better error; valid image")
+    });
+
+    let texture_atlas = Texture::from_images_2d(&g_ctx.device, &g_ctx.queue, &loaded_textures, Some("texture_atlas"))
+        .expect("TODO: better errors; valid textures");
 
     // bind group -> data constant through one draw call
     let bind_group_layout =
@@ -24,10 +48,10 @@ pub fn initialize_texture_atlas(g_ctx: UniqueView<GraphicsContext>, storages: Al
                     visibility: wgpu::ShaderStages::FRAGMENT, // use this bind group in the fragment shader
                     ty: wgpu::BindingType::Texture { // it's a texture, texture_2d<f32> in shader
                         multisampled: false,
-                        view_dimension: wgpu::TextureViewDimension::D2, // _2d
+                        view_dimension: wgpu::TextureViewDimension::D2Array, // _2d
                         sample_type: wgpu::TextureSampleType::Float { filterable: true },
                     },
-                    count: None,
+                    count: Some(NonZeroU32::new(loaded_textures.len() as _).expect("nonzero amount"))
                 },
                 wgpu::BindGroupLayoutEntry {
                     binding: 1,
@@ -46,16 +70,16 @@ pub fn initialize_texture_atlas(g_ctx: UniqueView<GraphicsContext>, storages: Al
             entries: &[ // matches the entries defined above
                 wgpu::BindGroupEntry {
                     binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&texture.view), // assign the data into the bind group
+                    resource: wgpu::BindingResource::TextureView(&texture_atlas.view), // assign the data into the bind group
                 },
                 wgpu::BindGroupEntry {
                     binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&texture.sampler),
+                    resource: wgpu::BindingResource::Sampler(&texture_atlas.sampler),
                 }
             ],
             label: Some("Texture Atlas Bind Group"),
         }
     );
 
-    storages.add_unique(TextureAtlas { texture, bind_group, bind_group_layout });
+    storages.add_unique(TextureAtlas { texture_atlas, bind_group, bind_group_layout });
 }
