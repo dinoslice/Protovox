b256ea457318d72e73700f8308e41601ee100717
abscosmos <joshua@dinoslice.com>
2024-12-18 10:45:25 -0600

    Create function to return item after breaking block

--- game/src/block/mod.rs
+++ game/src/block/mod.rs
@@ -1,6 +1,9 @@
+use std::num::NonZeroU8;
+use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
 use strum::{EnumCount, FromRepr};
 use crate::block::face_type::FaceType;
+use crate::item::{ItemStack, ItemType};
 
 pub mod face_type;
 
@@ -59,6 +62,23 @@ impl Block {
             Block::Log => LOG_TEXTURE,
             Block::Leaf => DEBUG_GREEN,
             Block::Stone => DEBUG_RED,
+        }
+    }
+
+    pub fn on_break(&self, /* break_context: BreakContext TODO: break context for fortune*/) -> Option<ItemStack> {
+        use Block as B;
+        use ItemType as I;
+
+        match self {
+            B::Air | B::Debug => None,
+            B::Grass | B::Dirt  => Some(I::Dirt.default_one()),
+            B::Cobblestone | B::Stone => Some(I::Cobblestone.default_one()),
+            B::Log => Some(I::Log.default_one()),
+            B::Leaf => {
+                let count = thread_rng().gen_range(5..15);
+
+                Some(I::LeafPile.default_stack(NonZeroU8::new(count).expect("0 is not in range")))
+            }
         }
     }
     
