5ff652c41fcfe61394ee68cfa77b33df9a639a4f
abscosmos <joshua@dinoslice.com>
2025-01-14 17:28:22 -0600

    Make ChunkPos::adjacent_to_face return Err with the wrapped value

--- game/src/chunk/adjacent.rs
+++ game/src/chunk/adjacent.rs
@@ -1,18 +1,11 @@
+use std::array;
 use crate::block::face_type::FaceType;
+use crate::chunk::CHUNK_SIZE;
 use crate::chunk::pos::ChunkPos;
 
 impl ChunkPos {
-    pub fn adjacent_to_face(&self, face: FaceType) -> Option<Self> {
-        use FaceType as FT;
-
-        let (dx, dy, dz): (i8, i8, i8) = match face {
-            FT::Bottom => (0, -1, 0),
-            FT::Top => (0, 1, 0),
-            FT::Front => (0, 0, 1),
-            FT::Back => (0, 0, -1),
-            FT::Left => (-1, 0, 0),
-            FT::Right => (1, 0, 0),
-        };
+    pub fn adjacent_to_face(&self, face: FaceType) -> Result<Self, Self> {
+        let [dx, dy, dz]: [i8; 3] = face.as_vector().into();
 
         let new_x = self.x()
             .wrapping_add(dx as u8);
@@ -23,6 +16,12 @@ impl ChunkPos {
         let new_z = self.z()
             .wrapping_add(dz as u8);
 
-        Self::new(new_x, new_y, new_z).ok()
+        Self::new(new_x, new_y, new_z).or_else(|_| {
+            let new = [new_x, new_y, new_z];
+
+            let [x, y, z] = array::from_fn(|i| new[i] % CHUNK_SIZE[i]);
+
+            Ok(ChunkPos::new(x, y, z).expect("in range"))
+        })
     }
 }
