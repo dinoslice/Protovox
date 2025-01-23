43134cd8074156f9451f7bc71c1ac8ff103849c3
abscosmos <joshua@dinoslice.com>
2025-01-23 11:49:27 -0600

    Create Identifier type using strck

---
+++ dino_plugins/src/ident.rs
@@ -0,0 +1,42 @@
+use std::sync::Arc;
+use strck::{Check, Ck, Invariant, ident::unicode};
+
+pub struct DinoIdent;
+
+pub type Ident = Ck<DinoIdent>;
+pub type IdentBuf<B = Arc<str>> = Check<DinoIdent, B>;
+
+#[derive(thiserror::Error, Debug)]
+pub enum DinoIdentError {
+    #[error("Identifier contained invalid character '{0}'")]
+    InvalidChar(char),
+    #[error("Cannot use wildcard as identifier")]
+    Wildcard,
+    #[error("Cannot have empty identifier")]
+    Empty,
+}
+
+impl Invariant for DinoIdent {
+    type Error = DinoIdentError;
+
+    fn check(str: &str) -> Result<(), Self::Error> {
+        fn check_inner(res: Result<&str, unicode::Error>) -> Result<(), DinoIdentError> {
+            match res {
+                Ok(str) => match str.chars().find(|c| !c.is_alphanumeric()) {
+                    Some(invalid) => Err(DinoIdentError::InvalidChar(invalid)),
+                    None => Ok(())
+                }
+                Err(unicode::Error::Start(c)) | Err(unicode::Error::Continue(c)) => Err(DinoIdentError::InvalidChar(c)),
+                Err(unicode::Error::Empty) => Err(DinoIdentError::Empty),
+            }
+        }
+
+        match unicode::UnicodeIdent::check(str) {
+            Err(unicode::Error::Start('_')) => match str.len() {
+                1 => Err(DinoIdentError::Wildcard), // `_` isn't ok
+                _ => check_inner(Ok(str.trim_start_matches('_'))),
+            }
+            res => check_inner(res.map(|_| str)),
+        }
+    }
+}
--- dino_plugins/src/lib.rs
+++ dino_plugins/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod engine;
+pub mod ident;
 
 pub trait DinoPlugin<P, I, M> {
     fn instructions(&self, phase: P) -> Option<I> {
