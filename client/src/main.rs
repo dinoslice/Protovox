8efe404cff9bf1d212966c2a4ba7f0575e68fa19
abscosmos <joshua@dinoslice.com>
2025-01-25 11:11:51 -0600

    Create & use visual debug plugin

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
 use engine::application::plugin_manager::PluginManager;
 use engine::VoxelEngine;
+use gizmos::GizmosPlugin;
+use visual_debug::VisualDebugPlugin;
 
 fn main() {
     client::init_tracing().expect("tracing initialized");
@@ -7,5 +9,7 @@ fn main() {
     client::run(
         PluginManager::new()
             .with(&VoxelEngine)
+            .with(&GizmosPlugin)
+            .with(&VisualDebugPlugin)
     );
 }
