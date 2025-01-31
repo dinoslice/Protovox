6c334d126ad3a6d92245a077056888be3bf7bb6e
abscosmos <joshua@dinoslice.com>
2025-01-31 11:06:50 -0600

    Implement both server & client deserializers

--- plugins/networking/src/lib.rs
+++ plugins/networking/src/lib.rs
@@ -1,3 +1,4 @@
+use std::any;
 use std::any::TypeId;
 use std::io::Read;
 use flate2::Compression;
@@ -6,15 +7,17 @@ use flate2::write::ZlibEncoder;
 use hashbrown::HashMap;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
-use shipyard::{AllStoragesViewMut, Component, EntityId};
-type Deserializer = fn(&[u8], AllStoragesViewMut) -> Option<EntityId>;
+use shipyard::{AllStorages, Component, EntityId, Unique, ViewMut};
 
-type TypeIdentifier = u16; // TODO: switch to NonZeroU16 for niche value optimization, wrapper type to eliminate incorrect usage, rename *Header
+type DeserializerClient = fn(&[u8], &mut AllStorages) -> Option<EntityId>;
+type DeserializerServer = fn(&[u8], EntityId, &mut AllStorages) -> Option<()>; // TODO: errors
+
+pub type TypeIdentifier = u16; // TODO: switch to NonZeroU16 for niche value optimization, wrapper type to eliminate incorrect usage, rename *Header
 // TODO: add phantom type to public api to force correctness
 
-#[derive(Debug, Default, Clone)]
-pub struct PacketRegistry {
-    map: Vec<Deserializer>, // TypeIdentifier -> Deserializer
+#[derive(Debug, Default, Clone, Unique)]
+pub struct PacketRegistry { // TODO: only needs one of DeserializerClient or DeserializerServer
+    map: Vec<(DeserializerClient, DeserializerServer)>, // TypeIdentifier -> Deserializer
     ids: HashMap<TypeId, TypeIdentifier>,
 }
 
@@ -25,20 +28,39 @@ impl PacketRegistry {
 
     // TODO: custom error
     // returns whether the packet is already registered
-    pub fn register<T: RuntimePacket + DeserializeOwned + Component, const C: bool>(&mut self) -> bool {
+    pub fn register<T: RuntimePacket + DeserializeOwned + Component, const C: bool, const B: bool>(&mut self) -> bool {
         let type_id = TypeId::of::<T>();
 
         if self.ids.contains_key(&type_id) {
             return true;
         }
 
-        let deserializer = |bytes: &[u8], mut storages: AllStoragesViewMut| {
+        let deserializer_client: DeserializerClient = |bytes, storages| {
             Some(storages.add_entity(<T as RuntimePacket>::deserialize::<C>(bytes)?))
         };
 
+        let deserializer_server: DeserializerServer = if B {
+            |bytes, id, storages| {
+                match storages.borrow::<ViewMut<EventBus<T>>>() {
+                    Ok(mut vm_evt_bus) => match vm_evt_bus.get_or_insert_with(id, Default::default) {
+                        Some(mut bus) => bus.0.push(<T as RuntimePacket>::deserialize::<C>(bytes)?),
+                        None => tracing::error!("Tried to insert {:?} event to {id:?}, but it was dead", any::type_name::<T>()),
+                    },
+                    Err(_) => tracing::error!("Failed to borrow event bus storage"),
+                }
+                Some(())
+            }
+        } else {
+            |bytes, id, storages| {
+                storages.add_component(id, <T as RuntimePacket>::deserialize::<C>(bytes)?);
+                Some(())
+            }
+        };
+
+
         let id = self.map.len();
 
-        self.map.push(deserializer);
+        self.map.push((deserializer_client, deserializer_server));
 
         let prev = self.ids.insert(type_id, id as _);
 
@@ -53,18 +75,30 @@ impl PacketRegistry {
             .copied()
     }
 
-    pub fn deserializer_for<T: 'static>(&self) -> Option<Deserializer> {
+    pub fn deserializer_for_id(&self, type_id: TypeIdentifier) -> Option<(DeserializerClient, DeserializerServer)> {
         self.map
-            .get(self.identifier_of::<T>()? as usize)
+            .get(type_id as usize)
             .copied()
     }
 
-    pub fn consume_packet_into(&self, bytes: &[u8], storages: AllStoragesViewMut) -> Option<EntityId> {
+    pub fn deserializer_for_ty<T: 'static>(&self) -> Option<(DeserializerClient, DeserializerServer)> {
+        self.deserializer_for_id(self.identifier_of::<T>()?)
+    }
+
+    pub fn client_consume_packet_into(&self, bytes: &[u8], storages: &mut AllStorages) -> Option<EntityId> { // TODO: type state
         let id = Self::identifier_from(bytes)?; // err: malformed data
 
-        let deserializer= self.map.get(id as usize)?; // err: unregistered type
+        let deserializer= self.map.get(id as usize)?.0; // err: unregistered type
 
         deserializer(bytes, storages) // err: deserialization error
+    }
+
+    pub fn server_consume_packet_into(&self, bytes: &[u8], entity_id: EntityId, storages: &mut AllStorages) -> Option<()> {
+        let id = Self::identifier_from(bytes)?; // err: malformed data
+
+        let deserializer= self.map.get(id as usize)?.1; // err: unregistered type
+
+        deserializer(bytes, entity_id, storages) // err: deserialization error
     }
 
     pub fn identifier_from(bytes: &[u8]) -> Option<TypeIdentifier> {
