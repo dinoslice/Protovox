2ab7c2ff42a219f4784941baad36ed0964a1533d
abscosmos <joshua@dinoslice.com>
2025-02-01 13:22:00 -0600

    Replace engine's EventBus with networking's EventBus

--- engine/src/events/event_bus.rs
+++ engine/src/events/event_bus.rs
@@ -1,10 +1,1 @@
-use shipyard::Component;
-
-#[derive(Component, Debug)]
-pub struct EventBus<T: Sync + Send + 'static>(pub Vec<T>);
-
-impl<T: Sync + Send + 'static> Default for EventBus<T> {
-    fn default() -> Self {
-        Self(Vec::default())
-    }
-}
+pub use ::networking::EventBus;
