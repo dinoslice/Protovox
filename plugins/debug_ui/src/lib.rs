70eca1e0f001c61b040b293635b87745c3fa6114
abscosmos <joshua@dinoslice.com>
2025-02-14 10:47:19 -0600

    Create egui rendering workload for debug ui plugin

--- plugins/debug_ui/src/lib.rs
+++ plugins/debug_ui/src/lib.rs
@@ -1,16 +1,38 @@
+use shipyard::{IntoWorkload, UniqueView, Workload};
 use strck::IntoCk;
 use dino_plugins::engine::{DinoEnginePlugin, EnginePluginMetadata};
-use egui_systems::EguiSystemsPlugin;
+use egui_systems::{CurrentEguiFrame, DuringEgui, EguiSystemsPlugin};
+use engine::networking::server_handler::ServerHandler;
 use engine::VoxelEngine;
 
 pub struct DebugUiPlugin;
 
 impl DinoEnginePlugin for DebugUiPlugin {
+    fn render(&self) -> Option<Workload> {
+        egui
+            .into_workload()
+            .order_egui()
+            .into()
+    }
     fn plugin_metadata(&self) -> EnginePluginMetadata {
         EnginePluginMetadata {
             name: "debug_ui".ck().expect("valid identifier"),
             version: "0.1.0",
             dependencies: &[&VoxelEngine, &EguiSystemsPlugin],
         }
+    }
+}
+
+fn egui(
+    egui_frame: UniqueView<CurrentEguiFrame>,
+
+    opt_server_handler: Option<UniqueView<ServerHandler>>,
+) {
+    if let Some(server_handler) = opt_server_handler {
+        egui::Window::new("Debug")
+            .show(egui_frame.ctx(), |ui| {
+                ui.heading("Server Handler");
+                ui.add(server_handler.as_ref());
+            });
     }
 }
