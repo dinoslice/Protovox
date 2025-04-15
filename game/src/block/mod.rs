7dda0b68bbbfc80522d410a0014529ce8f5db708
abscosmos <joshua@dinoslice.com>
2025-04-15 10:56:10 -0500

    Add data to blocks

--- game/src/block/mod.rs
+++ game/src/block/mod.rs
@@ -1,21 +1,22 @@
 use serde::{Deserialize, Serialize};
-use strum::{EnumCount, FromRepr};
-use crate::block::face_type::FaceType;
+use strum::{EnumCount, EnumDiscriminants};
+use crate::block::face_type::{Axis, FaceType};
 
 pub mod face_type;
 
 #[repr(u16)] // TODO: eventually replace strum::EnumCount with std::mem::variant_count
-#[derive(Clone, Copy, Eq, PartialEq, Default, Debug, Deserialize, Serialize, EnumCount, FromRepr)]
+#[derive(Clone, Eq, PartialEq, Default, Debug, Deserialize, Serialize, EnumCount, EnumDiscriminants)]
+#[strum_discriminants(name(BlockTy))]
 pub enum Block {
     #[default]
-    Air = 0,
+    Air,
     Grass,
     Dirt,
     Cobblestone,
     Stone,
-    Log,
+    Log { rotation: Axis },
     Leaf,
-    Debug
+    Debug,
 }
 
 #[repr(u8)]
@@ -30,7 +31,7 @@ pub enum TextureType {
 pub type TextureId = u8;
 
 impl Block {
-    pub const fn texture_id(&self, face_type: FaceType) -> Option<TextureId> {
+    pub fn texture_id(&self, face_type: FaceType) -> Option<TextureId> {
         const COBBLE_TEXTURE: TextureId = 0;
         const DIRT_TEXTURE: TextureId = 1;
         const GRASS_TOP_TEXTURE: TextureId = 2;
@@ -41,37 +42,41 @@ impl Block {
         const DEBUG_BLUE: TextureId = 7;
         const LOG_TEXTURE: TextureId = 8;
 
-        let id = match self {
-            Block::Air => return None,
-            Block::Grass => match face_type {
+        let id = match self.ty() {
+            BlockTy::Air => return None,
+            BlockTy::Grass => match face_type {
                 FaceType::Top => GRASS_TOP_TEXTURE,
                 FaceType::Bottom => DIRT_TEXTURE,
                 _ => GRASS_SIDE_TEXTURE,
             }
-            Block::Dirt => DIRT_TEXTURE,
-            Block::Cobblestone => COBBLE_TEXTURE,
-            Block::Debug => match face_type {
+            BlockTy::Dirt => DIRT_TEXTURE,
+            BlockTy::Cobblestone => COBBLE_TEXTURE,
+            BlockTy::Debug => match face_type {
                 FaceType::Left | FaceType::Right => DEBUG_RED,
                 FaceType::Bottom | FaceType::Top => DEBUG_BLUE,
                 FaceType::Front | FaceType::Back => DEBUG_GREEN,
             }
-            Block::Log => LOG_TEXTURE,
-            Block::Stone | Block::Leaf => todo!()
+            BlockTy::Log => LOG_TEXTURE,
+            BlockTy::Stone | BlockTy::Leaf => todo!()
         };
 
         Some(id)
     }
     
+    pub fn ty(&self) -> BlockTy {
+        self.into()
+    }
+    
     pub fn placeable(&self) -> bool {
-        match self {
-            Block::Air => false,
-            Block::Grass => true,
-            Block::Dirt => true,
-            Block::Cobblestone => true,
-            Block::Debug => true,
-            Block::Stone => true,
-            Block::Log => true,
-            Block::Leaf => true,
+        match self.ty() {
+            BlockTy::Air => false,
+            BlockTy::Grass => true,
+            BlockTy::Dirt => true,
+            BlockTy::Cobblestone => true,
+            BlockTy::Debug => true,
+            BlockTy::Stone => true,
+            BlockTy::Log => true,
+            BlockTy::Leaf => true,
         }
     }
 }
