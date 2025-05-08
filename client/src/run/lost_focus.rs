35250a33d38837a527e8c4c829f15eebd0af3311
abscosmos <joshua@dinoslice.com>
2025-05-08 10:41:41 -0500

    Pause the game on lost focus

--- client/src/run/lost_focus.rs
+++ client/src/run/lost_focus.rs
@@ -1,22 +1,13 @@
-use shipyard::{Unique, UniqueOrDefaultViewMut, UniqueView, UniqueViewMut};
+use shipyard::{AllStoragesView, Unique, UniqueOrDefaultViewMut, UniqueView, UniqueViewMut};
 use tracing::error;
 use engine::application::CaptureState;
+use engine::application::pause::{IsPaused, ToggleGuiPressed};
 use engine::input::InputManager;
 use engine::rendering::graphics_context::GraphicsContext;
+use engine::rendering::gui_bundle::GuiBundle;
 
-#[derive(Unique, Default)]
-pub struct LostFocus {
-    was_prev_captured: bool
-}
-
-pub fn on_window_focus_change(focused: bool, g_ctx: UniqueView<GraphicsContext>, mut lost_focus: UniqueOrDefaultViewMut<LostFocus>, mut capture_state: UniqueViewMut<CaptureState>, mut input_manager: UniqueViewMut<InputManager>) {
-    if !focused || lost_focus.was_prev_captured {
-        lost_focus.was_prev_captured = capture_state.is_captured();
-
-        if capture_state.set(&g_ctx.window, focused).is_none() {
-            error!("Unable to set capture/release mouse cursor.")
-        } else if !focused { // only reset action map if released cursor
-            input_manager.reset_all();
-        }
+pub fn on_window_focus_change(focused: bool, capture: UniqueView<CaptureState>, storages: AllStoragesView) {
+    if capture.is_captured() && !focused {
+        storages.add_unique(ToggleGuiPressed);
     }
 }
